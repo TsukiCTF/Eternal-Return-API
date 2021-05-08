7cf46dd3001ac9afd986d0e2f4a3c5d74f619cc1
TsukiCTF <tsukictf@gmail.com>
2021-05-07 23:35:20 -0400

    Added get_user_rank method to base client class

--- ERBSClient.py
+++ ERBSClient.py
@@ -24,3 +24,16 @@ class ErbsClient(object):
         r_data = json.loads(r_json)
         user_num = r_data['user']['userNum']
         return user_num
+
+    # Get user rank
+    def get_user_rank(self, user_num, season_id, matching_team_mode):
+        url = f'{self.api_url}/rank/{user_num}/{season_id}/{matching_team_mode}'
+        r = requests.get(url, headers=self.http_header)
+        r_json = json.dumps(r.json())
+        r_data = json.loads(r_json)
+        
+        if r_data['message'] == 'Success':
+            user_rank = r_data['userRank']
+            return user_rank
+            
+    
