9d6cf461986877d955c51d631c9df51c4a2eb2b0
TsukiCTF <tsukictf@gmail.com>
2021-05-29 23:09:22 -0400

    Minimized JSON'ing requests response

--- ERBSClient.py
+++ ERBSClient.py
@@ -19,8 +19,7 @@ class ErbsClient(object):
     def get_user_num(self, user_nickname):
         url = f'{self.api_url}/user/nickname/'
         r = requests.get(url, headers=self.http_header, params={('query', user_nickname),})
-        r_json = json.dumps(r.json())
-        r_data = json.loads(r_json)
+        r_data = r.json()
         user_num = r_data['user']['userNum']
         return user_num
         
@@ -28,22 +27,19 @@ class ErbsClient(object):
     def get_user_stats(self, user_num, season_id):
         url = f'{self.api_url}/user/stats/{user_num}/{season_id}'
         r = requests.get(url, headers=self.http_header)
-        r_json = json.dumps(r.json())
-        r_data = json.loads(r_json)
-        return r_data
+        r_json = r.json()
+        return r_json
             
     # Get user games
     def get_user_games(self, user_num):
         url = f'{self.api_url}/user/games/{user_num}'
         r = requests.get(url, headers=self.http_header)
-        r_json = json.dumps(r.json())
-        r_data = json.loads(r_json)
-        return r_data
+        r_json = r.json()
+        return r_json
             
     # Get game details
     def get_game_details(self, game_id):
         url = f'{self.api_url}/games/{game_id}'
         r = requests.get(url, headers=self.http_header)
-        r_json = json.dumps(r.json())
-        r_data = json.loads(r_json)
-        return r_data
+        r_json = r.json()
+        return r_json
